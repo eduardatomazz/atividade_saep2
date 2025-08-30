d853aa216f845616d2941f612074315405ae2c60
Eduarda <eduarda_tomaz@estudante.sesisenai.org.br>
2025-08-30 12:09:09 -0300

    add

---
+++ public/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="entrar">
+         <p>Bem Vindo</p>
+         <label for="email">E-mail</label>
+         <input type="text">
+         <label for="senha">Senha</label>
+         <input type="text">
+         <a href="/public/index.php">ENTRAR</a>
+    </div>
+
+</body>
+</html>
